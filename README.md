09ab471a5ab7ace86f1f0e80e7e776530f6a1e74
StepanLebedevu <stepan_leb@mail.ru>
2023-12-17 14:15:49 +0300

    changed markdown elements

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-TODO:   
-visualisation + 2 task visualization ??   
-вывод   
+### Сборка и запуск Docker   
+Собираем docker image:   
+    <code>docker build -t streamlitapp:latest . </code>   
+Запускаем docker container c собранным image:    
+    <code>docker run -p 8501:8501 streamlitapp:latest</code>   
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import scipy.stats as stats
 import streamlit as st
 
 
-
 def calcMinMax(data, arr):
     """
     Функция, считающая минимальные и максимальные аргументы для выбранных колонок pd.DataFrame для слайдеров st.slider()
@@ -54,8 +53,8 @@ def splitData(data, hypothesisNumber, work_days, age=0):
     Функция, разделяющая данные на две подвыборки, в зависимости от критерия гипотезы и полученным переменным
     :param data: выборка pd.DataFrame
     :param hypothesisNumber: выборка pd.Series
-    :param work_days: 
-    :param age:
+    :param work_days: гиперпараметр
+    :param age: гиперпараметр
     """
     work_days_split = data[data["Количество больничных дней"] > work_days]
     if (hypothesisNumber == "1 Гипотеза"):
@@ -68,6 +67,11 @@ def splitData(data, hypothesisNumber, work_days, age=0):
 
 
 def preprocessData(data):
+    """
+    Функция, предобрабатывающая DataFrame с кодировкой cp1521
+    Удаляет лишние кавычки
+    :param data: обьект pd.DataFrame
+    """
     data.replace(r'(\")', r"", regex=True, inplace=True)
     data.rename(columns=lambda x: re.sub('(\")','',x), inplace=True)
     data["Количество больничных дней"] = df["Количество больничных дней"].astype("int64")
@@ -77,68 +81,180 @@ def preprocessData(data):
     
    
 def loadData(file):
-     df = pd.read_csv(file, encoding='cp1251', sep=',', quoting=3)
-     return df
+    """
+    Функция загружает из csv файла обьект pd.DataFrame
+    :param file: путь к файлу
+    """
+    df = pd.read_csv(file, encoding='cp1251', sep=',', quoting=3)
+    return df
     
 
 def _markdownPval(text, pval1, pval2=0, alpha=0.05):
-    st.markdown(f"{text}   \np-value: {pval1}   \np-value: {pval2}")
+    """
+    Функция оберки двух p-значений в отформатированный markdown
+    """
+    st.markdown(f"###{text}   \np-value: {pval1}   \np-value: {pval2}")
     if (pval1 >= alpha and pval2 >= alpha):
         return False
     else:
         return True
 
+def _markdownSinglePval(text, pval1, alpha=0.05):
+    """
+    Функция оберки одного p-значения в отформатированный markdown
+    """
+    st.markdown(f"###{text}   \np-value: {pval1}")
+    if (pval1 >= alpha):
+        return False
+    else:
+        return True
     
 def testShapiro(data1, data2):
+    """
+    Функция проводит для двух выборок тесты Шапиро и возвращает их p-значения
+    :param data1: первая выборка pd.DataFrame
+    :param data2: вторая выборка pd.DataFrame
+    """
     results_1 = stats.shapiro(data1)
     results_2 = stats.shapiro(data2)
     _markdownPval("Результаты теста шапиро для выборок:", results_1.pvalue, results_2.pvalue)
     
-st.header('Тестовое задание на вакансию: Младший Исследователь данных (DS) / стажер')
-st.sidebar.header("Загрузка данных")
-file = st.sidebar.file_uploader("Убедитесь, что данные соответствуют предоставленному ранее формату")
-
-df = pd.DataFrame()
-
-if file:
-    df = loadData(file)
-    df = preprocessData(df)
-
-if not df.empty:
-    st.write("Загруженный датасет (С предопработкой из-за кодировки):", df)
-    but = st.sidebar.checkbox("Показать информацию о ваших данных")
-    if but:
-        st.sidebar.write("Кол-во обьектов  : " , df.shape[0])
-        st.sidebar.write("Кол-во признаков : " , df.shape[1])
-        st.sidebar.write("\nПропущенные значения :  ", df.isnull().sum().values.sum())
-        st.sidebar.write("\n      Уникальные значения :  \n", df.nunique())
-        st.sidebar.write("\n       Типизация колонок :  \n", df.dtypes)
-    st.header("Проверка гипотез")
-    selected_hypothesis = st.selectbox('Выберите гипотезу для проверки', ["1 Гипотеза","2 Гипотеза"])
     
-    min_age, max_age, min_days, max_days = calcMinMax(df, ["Возраст", "Количество больничных дней"])
-    if selected_hypothesis == "1 Гипотеза":
-        # age = st.slider('Возраст', min_value=min_age, max_value=max_age)
-        work_days = st.slider('Кол-во пропущенных дней', min_value=min_days, max_value=max_days)
-        st.write('Мужчины пропускают в течение года более ', work_days, 'рабочих дней по болезни значимо чаще женщин')
+    
+def testMannWhitney(data1, data2):
+    """
+    Функция проводит для двух выборок тесты Шапиро и возвращает их p-значения
+    :param data1: первая выборка pd.DataFrame
+    :param data2: вторая выборка pd.DataFrame
+    """
+    results = stats.mannwhitneyu(data1, data2)
+    _markdownPval("Результаты теста Манна-Уитни для выборок:", results.pvalue)
+    
+    
+def testStudent(data1, data2):
+    """
+    Функция проводит для двух выборок тесты Стьюдента и возвращает их p-значения
+    :param data1: первая выборка pd.DataFrame
+    :param data2: вторая выборка pd.DataFrame
+    """
+    results = stats.ttest_ind(data1, data2, equal_var=False)
+    _markdownSinglePval("Результаты теста Стьюдента для выборок:", results.pvalue)    
+
+    
+def renderHypothesis(hypothesis, work_days, age=0, alpha=0.05):
+    """
+    Функция выводит формальный markdown для гипотез
+    :param hypothesis: проверяемая гипотеза
+    :param work_days: гиперпараметр кол-ва дней
+    :param age=0: гиперпараметр возраст
+    :param alpha=0.05: гиперпараметр уровень значимости
+    """
+    if hypothesis == "2 Гипотеза":
+        st.markdown(f'### Формулировка гипотез:   \
+            \n1. Null Hypothesis $H_0$ - Старшие сотрудники (> {age} лет) пропускают в течение года более {work_days} рабочих дней по болезни **реже или сопоставимо** их более молодым коллегам (<= {age} лет).   \
+            \n2. Alternate Hypothesis $H_a$ - Старшие сотрудники (> {age} лет) пропускают в течение года более 2 рабочих дней по болезни значительно **чаще**, чем их более молодые коллеги (<= {age} лет).   \
+            \n### Формальная формулировка гипотез:   \
+            \n1. $H_0: \mu_1 <= \mu_2$   \
+            \n1. $H_a: \mu_1 > \mu_2$,    \
+            \nГде: $\mu_1$ - математическое ожидание выборки из старших сотрудников, $\mu_2$ - математическое ожидание выборки из молодых сотрудников   \
+            \n### Определение уровня значимости:   \
+            \nУровень значимости оставим прежним    \
+            \n$\alpha$ = {alpha}')
+    if hypothesis == "1 Гипотеза":
+        st.markdown(f'### Формулировка гипотез:   \
+            \n1. Null Hypothesis $H_0$ - Мужчины пропускают в течение года более {work_days} рабочих дней по болезни **реже или сопоставимо** женщинам.   \
+            \n2. Alternate Hypothesis $H_a$ - Мужчины пропускают в течение года более {work_days} рабочих дней по болезни значительно **чаще** женщин.   \   
+            \n### Формальная формулировка гипотез:   \
+            \n1. $H_0: \mu_1 <= \mu_2$   \
+            \n1. $H_a: \mu_1 > \mu_2$,    \
+            \nГде: $\mu_1$ - математическое ожидание выборки из старших сотрудников, $\mu_2$ - математическое ожидание выборки из молодых сотрудников   \
+            \n### Определение уровня значимости:   \
+            \nУровень значимости оставим прежним    \
+            \n$\alpha$ = {alpha}')
+        
+        
+def renderConclusion(hypothesis, test, testResult, work_days, age=0):
+    """
+    Функция выводит формальный markdown для гипотез
+    :param hypothesis: проверяемая гипотеза
+    :param test: статистический тест: MW | S
+    :param testResult: реузльтат теста: True | False
+    :param work_days: гиперпараметр кол-ва дней
+    :param age=0: гиперпараметр возраст
+    """
+    if test == "MW":
+        test_string = "Манна-Уитни"
     else:
-        age = st.slider('Возраст', min_value=min_age, max_value=max_age)
-        work_days = st.slider('Кол-во пропущенных дней', min_value=min_days, max_value=max_days)
-        st.write("Работники старше ",age," лет пропускают в течение года более ",work_days," рабочих дней по болезни значимо чаще своих более молодых коллег")
+        test_string = "Стьюдента"
+    if hypothesis == "1 Гипотеза":
+        if testResult:
+            st.markdown(f'### Вывод:   \
+                \n Тест {test_string} дал нам основания отвергнуть нулевую гипотезу, и мы имеем права сказать, что:   
+                \n**Мужчины пропускают в течение года более {work_days} рабочих дней по болезни значительно **чаще** женщин.**')
+        else:
+            st.markdown(f'### Вывод:   \
+                \n Тест {test_string} не дал нам оснований отвергнуть нулевую гипотезу, вывод:   
+                \n**Мужчины пропускают в течение года более {work_days} рабочих дней по болезни **реже или сопоставимо** женщинам.**')
+    if hypothesis == "2 Гипотеза":
+        if testResult:
+            st.markdown(f'### Вывод:   \
+                \n Тест {test_string} дал нам основания отвергнуть нулевую гипотезу, и мы имеем права сказать, что:   
+                \n**Старшие сотрудники (> {age} лет) пропускают в течение года более 2 рабочих дней по болезни значительно **чаще**, чем их более молодые коллеги (<= {age} лет).**')
+        else:
+            st.markdown(f'### Вывод:   \
+                \n Тест {test_string} не дал нам оснований отвергнуть нулевую гипотезу, вывод:   
+                \n**Старшие сотрудники (> {age} лет) пропускают в течение года более {work_days} рабочих дней по болезни **реже или сопоставимо** их более молодым коллегам (<= {age} лет).**')
     
-    start_button = st.button('Запустить проверку гипотезы')
-    
-    if start_button:
+def main():    
+    st.header('Тестовое задание на вакансию: Младший Исследователь данных (DS) / стажер')
+    st.sidebar.header("Загрузка данных")
+    file = st.sidebar.file_uploader("Убедитесь, что данные соответствуют предоставленному ранее формату")
+
+    df = pd.DataFrame()
+
+    if file:
+        df = loadData(file)
+        df = preprocessData(df)
+
+    if not df.empty:
+        st.write("Загруженный датасет (С предопработкой из-за кодировки):", df)
+        but = st.sidebar.checkbox("Показать информацию о ваших данных")
+        if but:
+            st.sidebar.write("Кол-во обьектов  : " , df.shape[0])
+            st.sidebar.write("Кол-во признаков : " , df.shape[1])
+            st.sidebar.write("\nПропущенные значения :  ", df.isnull().sum().values.sum())
+            st.sidebar.write("\n      Уникальные значения :  \n", df.nunique())
+            st.sidebar.write("\n       Типизация колонок :  \n", df.dtypes)
+        st.header("Проверка гипотез")
+        selected_hypothesis = st.selectbox('Выберите гипотезу для проверки', ["1 Гипотеза","2 Гипотеза"])
+        
+        min_age, max_age, min_days, max_days = calcMinMax(df, ["Возраст", "Количество больничных дней"])
         if selected_hypothesis == "1 Гипотеза":
-            names = ["Мужчины. Среднее", "Женщины. Среднее"]
-            df_1, df_2 = splitData(df, selected_hypothesis, work_days)
+            work_days = st.slider('Кол-во пропущенных дней', min_value=min_days, max_value=max_days)
+            renderHypothesis("1 Гипотеза", work_days=work_days)
         else:
-            names = [f"{age} и младше. Среднее", f"Старше {age} лет. Среднее"]
-            df_1, df_2 = splitData(df, selected_hypothesis, work_days, age)
-        twoHistograms(df_1, df_2, [work_days, max_days], names)
-        if (testShapiro(df_1, df_2)):
-            st.write("Оба распределения близки к нормальному, для проверки гипотезы можем использовать t-test Сть")
-        else:
-            st.write("Оба (или одно) распределения далеки от нормального, нет возможности использовать t-test, используем тест Манна-уитни")
+            age = st.slider('Возраст', min_value=min_age, max_value=max_age)
+            work_days = st.slider('Кол-во пропущенных дней', min_value=min_days, max_value=max_days)
+            renderHypothesis("2 Гипотеза", work_days=work_days, age=age)
+        
+        start_button = st.button('Запустить проверку гипотезы')
+        
+        if start_button:
+            if selected_hypothesis == "1 Гипотеза":
+                names = ["Мужчины. Среднее", "Женщины. Среднее"]
+                df_1, df_2 = splitData(df, selected_hypothesis, work_days)
+            else:
+                names = [f"{age} и младше. Среднее", f"Старше {age} лет. Среднее"]
+                df_1, df_2 = splitData(df, selected_hypothesis, work_days, age)
+            twoHistograms(df_1, df_2, [work_days, max_days], names)
+            if (testShapiro(df_1, df_2)):
+                st.write("Оба распределения близки к нормальному, для проверки гипотезы можем использовать t-test Стьюдента")
+                test_result = testStudent(df_1, df_2)
+                renderConclusion(selected_hypothesis, "S", test_result, work_days, age)
+            else:
+                st.write("Оба (или одно) распределения далеки от нормального, нет возможности использовать t-test, используем тест Манна-уитни")
+                test_result = testMannWhitney(df_1, df_2)
+                renderConclusion(selected_hypothesis, "MW", test_result, work_days, age)
             
             
+main()
